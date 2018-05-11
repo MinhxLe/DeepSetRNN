10639d12dfbb883825299c567ae79d673db2be55
Minh Le <minh.d.le27@gmail.com>
2018-05-11 13:29:29 -0700

    cleaned up code and added logging

--- src/main.py
+++ src/main.py
@@ -3,71 +3,58 @@ import numpy as np
 import tensorflow as tf
 from model import BasicRNNClassifier
 import data
+import argparse
 import logging
-#TODO add logging
-#TODO add argparse
-#TODO fix flags
 ####FLAGS####
-flags = tf.app.flags
-#model flags
-#TODO edit help message
-flags.DEFINE_string('name',"BasicRNN", "Model Name")
-flags.DEFINE_integer("input_size", 28, "Epoch to train [30]")
-flags.DEFINE_integer("timesteps", 28, "Epoch to train [30]")
-flags.DEFINE_integer("hidden_size", 128, "Epoch to train [30]")
-flags.DEFINE_integer("output_size",10, "Epoch to train [30]")
-#training flags
-flags.DEFINE_boolean("load", False, "True for loading prior model, \
-        False for testing [False]")
-flags.DEFINE_boolean("train", True, "True for training, \
-        False for testing [False]")
-flags.DEFINE_integer("epoch", 100, "Epoch to train [30]")
-flags.DEFINE_float("learning_rate", 0.01, "Learning rate")
-flags.DEFINE_float("momentum", 0.9, "momentum")
-flags.DEFINE_integer("batch_size", 128, "The size of batch images [200]")
-#dir flags
-flags.DEFINE_string("models_dir", "../model", \
-        "Directory name to save the checkpoints [checkpoint]")
-flags.DEFINE_string("data_dir", "../data", 
-        "Directory name to save the checkpoints [checkpoint]")
-FLAGS = flags.FLAGS
-
+ap = argparse.ArgumentParser()
+#model args
+ap.add_argument('--name',type=str,default="RNN")
+ap.add_argument('--input_size',type=int,default=28)
+#TODO compute this or make it not static
+ap.add_argument('--timesteps',type=int,default=28) 
+ap.add_argument('--hidden_size',type=int,default=128)
+ap.add_argument('--output_size',type=int,default=10)
+#training args
+ap.add_argument('--load',type=bool,default=False)
+ap.add_argument('--train',type=bool,default=True)
+ap.add_argument('--epoch',type=int,default=100)
+ap.add_argument('--batch_size',type=int,default=True)
+ap.add_argument('--save_freq',type=int,default=10)
+#hyperparameters
+ap.add_argument('--learning_rate',type=float,default=0.01)
+ap.add_argument('--momentum',type=float,default=0.9)
 #debug/log flags
-flags.DEFINE_boolean("debug", True, "debug mode")
-flags.DEFINE_integer("save_freq",10, "Epoch to train [30]")
+ap.add_argument('--verbose',type=bool,default=True)
+ap.add_argument('--debug',type=bool,default=False)
+ap.add_argument('--log',type=bool,default=True)
+#directory
+#dir flags
+ap.add_argument('--models_dir',type=str,default='../model')
+ap.add_argument('--logs_dir',type=str,default="../log")
+ap.add_argument('--data_dir',type=str,default='../data')
 
-####LOGGING####
-logger = logging.getLogger(FLAGS.name)
-logger.setLevel(logging.INFO)
 
-console = logging.StreamHandler()
-console.setLevel(logging.INFO)
-logger.addHandler(console)
-
-def main(_):
+def main():
     logger.info(FLAGS)
-    #reading data
-    model_dir = os.path.join(FLAGS.models_dir,FLAGS.name)
+    ckpt_dir = os.path.join(FLAGS.models_dir,FLAGS.name)
+    model_fname = os.path.join(ckpt_dir,FLAGS.name)
     with tf.Session() as sess:
         model = BasicRNNClassifier(FLAGS)
         saver = tf.train.Saver()
-        #load
         epoch = 0
+        #loading existing model in if need be
         if FLAGS.load:
-            #TODO move to seperate function
-            logger.info("loading existing model")
-            saver.restore(sess,os.path.join(model_dir,FLAGS.name))
+            #TODO check if this is successful
+            saver.restore(sess,model_fname)
             epoch = tf.train.global_step(sess)
+            logger.info("loading existing model at epoch {}".format(epoch))
         else:
             sess.run(tf.global_variables_initializer())
-        #train 
-        if not os.path.isdir(model_dir):
-            os.makedirs(model_dir)
         if FLAGS.train:
-            logger.info("training")
+            logger.info("Training")
             max_epoch = epoch + FLAGS.epoch
             for epoch in range(epoch,max_epoch):
-                #validating and saving every frequence
+                #validating and saving every save_freq epoch
                 if epoch % FLAGS.save_freq == 0: 
                     n_batch = 0
                     accuracy = 0
@@ -79,9 +66,9 @@ def main(_):
                         predict = np.argmax(predict,axis=1)
                         accuracy += np.mean(predict == y_batch)
                         n_batch  += 1
-                    logger.info("validation acc: {}".format(accuracy/n_batch))
-                    saver.save(sess,os.path.join(model_dir,FLAGS.name))
-                
+                    logger.info("epoch: {}, val_acc: {}".format(epoch,accuracy/n_batch))
+                    saver.save(sess,model_fname)
+                #optimizing over training data
                 n_batch = 0
                 epoch_loss = 0
                 for X_batch, y_batch in data.iterate_minibatches(train_images,train_labels, 
@@ -90,8 +77,27 @@ def main(_):
                             feed_dict={ model.X : X_batch,model.y : y_batch})
                     epoch_loss += loss
                     n_batch += 1
-                logger.info("epoch:{} loss:{}".format(epoch,epoch_loss/n_batch))
+                logger.info("epoch: {}, train_loss: {}".format(epoch,epoch_loss/n_batch))
+        #final validation
+        n_batch = 0
+        accuracy = 0
+        loss = 0
+        for X_batch,y_batch in data.iterate_minibatches(test_images, test_labels,
+                FLAGS.batch_size,True):
+            predict,batch_loss = sess.run([model.prediction,model.loss],
+                    feed_dict = { model.X : X_batch, model.y : y_batch})
+            
+            predict = np.argmax(predict,axis=1)
+            accuracy += np.mean(predict == y_batch)
+            loss += batch_loss
+            n_batch  += 1
+        logger.info("final_loss: {}, final_acc: {}".format(loss/n_batch,accuracy/n_batch))
+        saver.save(sess,model_fname)
+        
 if __name__ == '__main__':
+    #parsing arguments
+    FLAGS = ap.parse_args() 
+    print(FLAGS)
     #getting dataset
     train_images = data.load_MNIST_images(FLAGS.data_dir)
     test_images  = data.load_MNIST_images(FLAGS.data_dir,True)
@@ -100,5 +106,28 @@ if __name__ == '__main__':
     
     train_labels = data.load_MNIST_labels(FLAGS.data_dir)
     test_labels  = data.load_MNIST_labels(FLAGS.data_dir,True)
+    #TODO setting model name with hyperparameter
     
-    tf.app.run()
+    #setting up logger
+    logger = logging.getLogger(FLAGS.name)
+    if FLAGS.log:
+        model_log_fname = os.path.join(FLAGS.logs_dir,FLAGS.name)
+        if not os.path.isdir(FLAGS.logs_dir):
+            os.makedirs(log_dir)
+        f_stream = logging.FileHandler(model_log_fname,mode='a')
+        logger.addHandler(f_stream) 
+    if FLAGS.verbose:
+        console = logging.StreamHandler()
+        logger.addHandler(console)
+    if FLAGS.debug:
+        console.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+    
+    #checkpoint dir
+    ckpt_dir = os.path.join(FLAGS.models_dir, FLAGS.name)
+    if not os.path.isdir(ckpt_dir):
+        os.makedirs(ckpt_dir)
+    
+    main()
+
